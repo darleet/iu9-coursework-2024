b69d24a098b5aca6a9d4b93eb6a2538fb3d1c9c5
darleet <darleet@mail.ru>
2024-12-25 14:21:21 +0300

    improve typing

--- app/core/config.py
+++ app/core/config.py
@@ -1,17 +1,15 @@
-from typing import List, Union
-
 from pydantic import AnyHttpUrl, BaseSettings, validator
 
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "coursework"
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    BACKEND_CORS_ORIGINS: list[AnyHttpUrl] = []
     APP_HOST: str = "localhost"
     APP_PORT: int = 8000
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     @classmethod
-    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+    def assemble_cors_origins(cls, v: str | list[str]) -> str | list[str]:
         if isinstance(v, str) and not v.startswith("["):
             return [i.strip() for i in v.split(",")]
         elif isinstance(v, (list, str)):
